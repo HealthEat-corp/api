5bde7aad903a644d828126e89c180a27f4808cd2
JungwooSim <cscd053@gmail.com>
2021-08-07 12:41:43 +0900

    project init

---
+++ src/main/kotlin/com/healtheat/api/HealtheatApiApplication.kt
@@ -0,0 +1,11 @@
+package com.healtheat.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HealtheatApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<HealtheatApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/healtheat/api/HealtheatApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.healtheat.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HealtheatApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
