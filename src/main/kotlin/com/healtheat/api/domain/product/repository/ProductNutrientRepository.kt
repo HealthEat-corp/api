b32c4adc6ee01ad8344510f2b3cf2c2f42967980
JungwooSim <cscd053@gmail.com>
2021-08-17 00:28:31 +0900

    버그 수정

--- src/main/kotlin/com/healtheat/api/domain/product/repository/ProductNutrientRepository.kt
+++ src/main/kotlin/com/healtheat/api/domain/product/repository/ProductNutrientRepository.kt
@@ -1,8 +1,9 @@
 package com.healtheat.api.domain.product.repository
 
+import com.healtheat.api.domain.product.ProductNutrient
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ProductNutrientRepository : JpaRepository<ProductNutrientRepository, Long> {
+interface ProductNutrientRepository : JpaRepository<ProductNutrient, Long> {
 }
