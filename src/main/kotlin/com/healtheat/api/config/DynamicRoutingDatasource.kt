c0ecef1c33800c3a0313f390e96d4860413781c4
JungwooSim <cscd053@gmail.com>
2021-08-17 00:28:17 +0900

    접근 제어자 수정

--- src/main/kotlin/com/healtheat/api/config/DynamicRoutingDatasource.kt
+++ src/main/kotlin/com/healtheat/api/config/DynamicRoutingDatasource.kt
@@ -3,7 +3,7 @@ package com.healtheat.api.config
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
 import org.springframework.transaction.support.TransactionSynchronizationManager
 
-class DynamicRoutingDatasource : AbstractRoutingDataSource() {
+open class DynamicRoutingDatasource : AbstractRoutingDataSource() {
 
     @Override
     override fun determineCurrentLookupKey(): Any? {
